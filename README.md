7092cc5788b64c51b09cd372af7d946d46e96314
SpaceDave1337 <73401687+SpaceDave1337@users.noreply.github.com>
2024-07-25 20:40:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projekt-Schild
+ A small mod for all of your sign needs
---
+++ davesignco.1.20.6/src/main/java/spacedave/davesignco/DaveSignCoClient.java
@@ -0,0 +1,10 @@
+package spacedave.davesignco;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DaveSignCoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
